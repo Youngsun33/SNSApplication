ebaae377ed00bc90bece30e11b6fbba3b6b7f41e
youngsun33 <heoyoungsun33@gmail.com>
2022-07-22 21:33:14 +0900

    Test commit

---
+++ app/src/main/java/com/example/snsapplication/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.snsapplication
+//게시글 쓰는 창
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
